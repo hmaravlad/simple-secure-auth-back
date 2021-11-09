43e23f836e60c1f8c7c41f3dda776b10bbe57337
hmaravlad <hmara.vladislav2@gmail.com>
2021-11-09 19:26:09 +0200

    add empty auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+  register(registerDto: RegisterDto) {
+    return 'Register';
+  }
+
+  login(loginDto: LoginDto) {
+    return `Login`;
+  }
+
+  logout() {
+    return `Logout`;
+  }
+
+}
